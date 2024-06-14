868e3ea319c680abc52baf0de423946ebebd06fc
Joris Mertz <info@jorismertz.nl>
2024-06-14 18:28:14 +0200

    wip logging refactor

--- go.mod
+++ go.mod
@@ -7,3 +7,10 @@ require (
 	github.com/mattn/go-sqlite3 v1.14.22
 	github.com/pelletier/go-toml/v2 v2.2.2
 )
+
+require (
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/rs/zerolog v1.33.0 // indirect
+	golang.org/x/sys v0.21.0 // indirect
+)
--- main.go
+++ main.go
@@ -7,9 +7,11 @@ import (
 	"jorismertz/lightspeed-dhl/database"
 	"jorismertz/lightspeed-dhl/dhl"
 	"jorismertz/lightspeed-dhl/lightspeed"
-	"log"
 	"net/http"
 	"os"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
 
 const (
@@ -17,49 +19,48 @@ const (
 )
 
 func main() {
-	database.Initialize()
-	order, err := lightspeed.GetOrder(274989157)
-	if err != nil {
-		panic(err)
-	}
-	pretty, err := json.MarshalIndent(order, "", "    ")
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(string(pretty))
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 
-	os.Exit(1)
+	database.Initialize()
+
+	// os.Exit(1)
 	dhl.StartPolling()
 
 	http.HandleFunc("/webhook", func(w http.ResponseWriter, r *http.Request) {
+		log.Debug().Str("Method", r.Method).Msg("Received webhook")
 		if r.Method == "POST" {
 			body, err := io.ReadAll(r.Body)
 			if err != nil {
-				panic(err)
+				log.Err(err).Msg("Failed to read request body")
+				return
 			}
 			var orderData lightspeed.IncomingOrder
 			err = json.Unmarshal(body, &orderData)
 			if err != nil {
-				panic(err)
+				log.Err(err).Msg("Failed to unmarshal request body")
+				return
 			}
 
 			draft := dhl.WebhookToDraft(orderData)
 
 			// err = dhl.CreateDraft(&draft)
 			// if err != nil {
-			// 	panic(err)
+			// log.Fatal().Err(err).Msg("Failed to create draft in DHL")
+			// return
 			// }
 
 			err = database.CreateDraft(draft.Id, *draft.OrderReference, orderData.Order.Number)
 
 			if err != nil {
-				panic(err)
+				log.Err(err).Msg("Failed to create draft in database")
+				return
 			}
 
 			w.WriteHeader(http.StatusOK)
 		}
 	})
 
-	fmt.Printf("Server listening on port %d\n", port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
+	log.Info().Int("Port", port).Msg("Starting server")
+	_ = http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 }
